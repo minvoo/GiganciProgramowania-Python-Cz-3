33bbf0119fdf70de2254f881f501794adfb44bf1
Mariusz Maciejewski <maciejewski_mariusz@wp.pl>
2024-10-29 19:52:03 +0100

    l6

---
+++ Lekcja 6/l6.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+tablica  = np.array([[-1,-2,-3], [4,5,6], [7,8,9]])
+
+print(tablica)
+print(f"Pierwszy element tablicy to {tablica[0]}")
+print(f"Pierwszy zagniezdzony elemnt to {tablica[0][0]}")
+print(f"Typ obiektu tablicy {type(tablica)}")
+print(f"Ksztalt tablicy : {tablica.shape}")
+
+tablica.
